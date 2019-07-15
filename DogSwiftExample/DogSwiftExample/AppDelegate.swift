4f7d54179137182413639ed123a364a514add026
Fabian Tinsz <fabian.tinsz@dreipol.ch>
2019-07-15 13:54:04 +0200

    Add Dreilog pod to project

--- DogSwiftExample/DogSwiftExample/AppDelegate.swift
+++ DogSwiftExample/DogSwiftExample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Dreilog
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ DogSwiftExample/Pods/Target Support Files/Pods-DogSwiftExample/Pods-DogSwiftExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogSwiftExample : NSObject
+@end
+@implementation PodsDummy_Pods_DogSwiftExample
+@end
