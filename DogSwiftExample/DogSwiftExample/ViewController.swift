843f03660b2395a519f3bd9aac804e763bfb7492
Fabian Tinsz <fabian.tinsz@dreipol.ch>
2019-07-15 14:16:49 +0200

    Log various functions

--- DogSwiftExample/DogSwiftExample/ViewController.swift
+++ DogSwiftExample/DogSwiftExample/ViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import Dreilog
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        Log.debug(#function, tag: .ui)
     }
-
-
 }
 
