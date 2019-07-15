42c8d4971c5a54b5cc2f337643170f8a213516c9
Fabian Tinsz <fabian.tinsz@dreipol.ch>
2019-07-15 13:40:47 +0200

    Init new Xcode project

---
+++ DogSwiftExample/DogSwiftExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogSwiftExample
+//
+//  Created by Fabian Tinsz on 15.07.19.
+//  Copyright © 2019 dreipol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
