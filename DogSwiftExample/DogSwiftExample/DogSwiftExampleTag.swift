515fdf4a4f4712e647187f85d117f44ebcecf71e
Fabian Tinsz <fabian.tinsz@dreipol.ch>
2019-08-20 12:04:06 +0200

    Add DogSwiftExampleTag

---
+++ DogSwiftExample/DogSwiftExample/DogSwiftExampleTag.swift
@@ -0,0 +1,19 @@
+//
+//  DogSwiftExampleTag.swift
+//  DogSwiftExample
+//
+//  Created by Fabian Tinsz on 20.08.19.
+//  Copyright © 2019 dreipol. All rights reserved.
+//
+
+import DogSwift
+
+enum ExampleTag: String {
+    case viewDidLoad
+}
+
+extension ExampleTag: TagProtocol {
+    func makeString() -> String {
+        return String(describing: self)
+    }
+}
